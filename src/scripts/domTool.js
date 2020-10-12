da8eae62fa0b782be4892c1b258b1b619260ac40
Rory Hellier <rory.hellier@googlemail.com>
2020-10-12 22:04:09 +0100

    Added domTools generators:
     - Added elementGen for creation of html elements
     - Added new version of textGen which creates the htmlTag and parses the texts spaces all in one

---
+++ src/scripts/domTool.js
@@ -0,0 +1,26 @@
+const elementGen = (tag, className = null, idName = null) => {
+  const element = document.createElement(tag);
+  if (className) { element.classList.add(className) };
+  if (idName) { element.id = id };
+  return element;
+}
+
+const textGen = (tag, text, symbol, className = null, idName = null) => {
+  const element = document.createElement(tag);
+  const unparsedText = text.split('');
+  const spacedText = [];
+  unparsedText.forEach(char => {
+    if (char === symbol) {
+      spacedText.push(' ');
+    } else {
+      spacedText.push(char);
+    }
+  })
+  const parsedText = spacedText.join('');
+  element.innerHTML = parsedText;
+  if (className) { element.classList.add(className) };
+  if (idName) { element.id = idName };
+  return element;
+}
+
+export { elementGen, textGen }
